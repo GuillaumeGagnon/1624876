ff2938259ae353d44ac15ce65e4f333af0089b90
Guillaume Gagnon <arkapin18@gmail.com>
2018-08-28 17:18:24 -0400

    Ajout des Log.d et création de fichier ressources.

--- app/src/main/java/cours5b5/guillaumegagnon/Parametres.java
+++ app/src/main/java/cours5b5/guillaumegagnon/Parametres.java
@@ -11,6 +11,21 @@ public class Parametres extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametres);
 
-        Log.d("MonEtiquette","Bonjour!");
+        Log.d("@",this.getResources().getString(R.string.messageLog));
+        Log.d("@@",this.getResources().getString(R.string.messageOrientationLog));
+
+    /*
+    NE FONCTIONNE PAS CAR LA RESSOURCE EXISTE SEULEMENT EN PORTRAIT
+
+    if(this.getResources().getBoolean(R.bool.portrait)){
+        Log.d("@@@", "PORTRAIT");
+    } else{
+        Log.d("@@@", "LANDSCAPE");
+    }
+
+
+    */
+
+
     }
 }
