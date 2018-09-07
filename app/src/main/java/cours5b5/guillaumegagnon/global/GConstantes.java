95565af71d6af819efc519594b5cd2d133ca61b0
Arkapin <arkapin18@gmail.com>
2018-09-07 01:58:35 -0400

    FAIT: * Codage du spinner hauteur TODO: * Coder les autres spinners

--- app/src/main/java/cours5b5/guillaumegagnon/global/GConstantes.java
+++ app/src/main/java/cours5b5/guillaumegagnon/global/GConstantes.java
@@ -2,8 +2,8 @@ package cours5b5.guillaumegagnon.global;
 
 public class GConstantes {
 
-    final int hMin = 4, hMax = 10, hDefault = 6;
-    final int lMin = 4, lMax = 10, lDefault = 7;
-    final int gagnerMin = 3, gagnerMax = 4, gagnerDefault = 4;
+    public static final int hMin = 4, hMax = 10, hDefault = 6;
+    public static final int lMin = 4, lMax = 10, lDefault = 7;
+    public static final int gagnerMin = 3, gagnerMax = 4, gagnerDefault = 4;
 
 }
--- app/src/main/java/cours5b5/guillaumegagnon/vues/VParametres.java
+++ app/src/main/java/cours5b5/guillaumegagnon/vues/VParametres.java
@@ -3,6 +3,12 @@ package cours5b5.guillaumegagnon.vues;
 import android.content.Context;
 import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import cours5b5.guillaumegagnon.R;
+import cours5b5.guillaumegagnon.global.GConstantes;
 
 public class VParametres extends ConstraintLayout {
     public VParametres(Context context) {
@@ -15,5 +21,35 @@ public class VParametres extends ConstraintLayout {
 
     public VParametres(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+
+        Log.d("@@@","pslpslpslpslpsl");
+
+
+        Spinner spinnerHauteur = this.findViewById(R.id.spinnerHauteur);
+        Spinner spinnerLargeur = this.findViewById(R.id.spinnerLargeur);
+        Spinner spinnerGagner = this.findViewById(R.id.spinnerGagner);
+
+        ArrayAdapter<Integer> adapterHauteur = new ArrayAdapter<>(this.getContext(), R.layout.support_simple_spinner_dropdown_item);
+        //spinnerHauteur.setAdapter(adapterHauteur);
+
+        ArrayAdapter<Integer> adapterLargeur = new ArrayAdapter<>(this.getContext(), R.layout.support_simple_spinner_dropdown_item);
+        spinnerHauteur.setAdapter(adapterLargeur);
+
+        ArrayAdapter<Integer> adapterGagner = new ArrayAdapter<>(this.getContext(), R.layout.support_simple_spinner_dropdown_item);
+        spinnerHauteur.setAdapter(adapterGagner);
+
+        for (int i = GConstantes.hMin; i <= GConstantes.hMax; i++){
+            adapterHauteur.add(i);
+        }
+        spinnerHauteur.setAdapter(adapterHauteur);
+        spinnerHauteur.setSelection(adapterHauteur.getPosition(GConstantes.hDefault));
+
+
+
     }
 }
